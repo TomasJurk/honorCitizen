2cddfc19a5fc88e9485ae99660160a32dad49ec7
TomasJurk <tomasstj@gmail.com>
2018-07-10 10:30:29 +0300

    maps deleted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { } from '@types/googlemaps';
-
-declare var google: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,27 +7,8 @@ declare var google: any;
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild('gmap') gmapElement: any;
-  map: google.maps.Map;
-
   constructor() { }
 
-  ngOnInit() {
-    const mapProp = {
-      center: new google.maps.LatLng(55.174692, 23.888514),
-      zoom: 7.9,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true,
-      gestureHandling: 'none',
-      zoomControl: false,
-      styles: [{
-        featureType: 'all',
-        elementType: 'labels',
-        stylers: [
-          { visibility: 'off' }
-        ]
-      }]
-    };
-    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-  }
+  ngOnInit() {}
+
 }
